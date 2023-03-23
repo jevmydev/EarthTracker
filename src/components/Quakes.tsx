e58e2569e608feca9ce438251a9a9eef85eb4039
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-23 15:39:34 -0300

    Feat loading and error management

--- src/components/Quakes.tsx
+++ src/components/Quakes.tsx
@@ -2,19 +2,23 @@ import { useQuakes } from "../hooks/useQuakes";
 
 import QuakesHero from "./QuakesHero";
 import QuakesList from "./QuakesList";
+import Loading from "./Loading";
+import Error from "./Error";
 
 export const Quakes = () => {
-    const { quakes, heroQuake, updateHeroQuake } = useQuakes();
+    const { quakes, heroQuake, loading, error, updateHeroQuake } = useQuakes();
 
     return (
         <section>
             <div className="max-w-screen-md mx-auto p-4 pt-6 flex flex-col gap-6 justify-between md:flex-row">
-                {quakes && heroQuake && (
+                {quakes && heroQuake && !loading && !error && (
                     <>
                         <QuakesHero heroQuake={heroQuake} />
                         <QuakesList quakes={quakes} updateHeroQuake={updateHeroQuake} />
                     </>
                 )}
+                {loading && <Loading />}
+                {error && <Error />}
             </div>
         </section>
     );
--- src/hooks/useQuakes.ts
+++ src/hooks/useQuakes.ts
@@ -5,6 +5,8 @@ import { type QuakesContractTypes, type ListQuakesContract } from "../types";
 export const useQuakes = () => {
     const [quakes, setQuakes] = useState<ListQuakesContract | null>(null);
     const [heroQuake, setHeroQuake] = useState<QuakesContractTypes | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const updateHeroQuake = (id: string) => {
         const selectedQuake = quakes?.filter((quake) => quake.id === id)?.at(0);
@@ -15,13 +17,18 @@ export const useQuakes = () => {
 
     const updateQuakes = async () => {
         try {
+            setLoading(true);
+
             const newQuakes = await getAllQuakes();
             const defaultHeroQuake = newQuakes?.at(0) ?? null;
 
             setQuakes(newQuakes);
             setHeroQuake(defaultHeroQuake);
         } catch (err) {
+            setError(`Fetching to quakes API: ${err}`);
             throw new Error(`Fetching to quakes API: ${err}`);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -30,5 +37,5 @@ export const useQuakes = () => {
         execQuakes();
     }, []);
 
-    return { quakes, heroQuake, updateQuakes, updateHeroQuake };
+    return { quakes, heroQuake, loading, error, updateQuakes, updateHeroQuake };
 };
