e2445c4c45e4293c8313b7e68317b4c8f22d1edc
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-23 15:38:48 -0300

    Add error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,9 @@
+export const Error = () => {
+    return (
+        <div className="h-96 w-full bg-slate-200 rounded p-4 text-4xl">
+            <p>Lo sentimos, ha ocurrido un error.</p>
+        </div>
+    );
+};
+
+export default Error;
