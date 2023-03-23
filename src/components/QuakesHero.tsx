ea485e8b884c3136b4392791cb8ccaca96e975ea
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-23 17:28:14 -0300

    Update responsive

--- src/components/QuakesHero.tsx
+++ src/components/QuakesHero.tsx
@@ -10,7 +10,7 @@ export const QuakesHero = ({ heroQuake }: QuakesHeroProps) => {
     return (
         <div>
             <article className="flex flex-col gap-6">
-                <header className="flex items-center gap-2">
+                <header className="flex flex-col items-start gap-2 sm:items-center sm:flex-row">
                     <h2 className="font-bold text-5xl">Último sismo</h2>
                     <span className="animate-ping p-1 rounded-full bg-indigo-300"></span>
                 </header>
